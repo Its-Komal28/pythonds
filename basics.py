babee2f608dc0cb2f15a0f59d9c2b353b2681245
Komal <itskomal2829@gmail.com>
2025-06-16 12:32:23 +0530

    DONE

---
+++ basics.py
@@ -0,0 +1,99 @@
+naam=input("enter name")
+for i in range(0,len(naam)):
+    print(naam)
+#printing star pattern...
+for i in range(5):
+    for j in range(0,i+1):
+        print("*",end="")
+    print()
+#practicing star questions...
+n=5
+for i in range(n):
+    for j in range(n):
+        print("*",end="  ")
+    print()
+n=5
+for i in range(n):
+    for j in range(n):
+        print("*",end="")
+    print(n)
+#increasing triangle pattern
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+#decreasing triangle pattern
+n=5
+for i in range(n):
+    for j in range(n,i,-1):
+        print("*",end=" ")
+    print()
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print("@",end=" ") 
+    print() 
+#right sided triangle
+
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end="")
+    
+    
+    for j in range(i+1):
+        print("*",end="")
+    print()
+#right sided triangle
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end='')
+    for j in range(i,n):
+        print("*",end="")
+    print()
+#hill pattern
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    for j in range(i+1):
+        print("*",end="")
+    print()
+#reverse hill pattern
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end="")
+    for j in range(i,n):
+        print("*",end="")
+    for j in range(i,n-1):
+        print("*",end="")
+    print()
+#diamond pattern
+n=5
+for i in range(n-1):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    for j in range(i,n-1):
+        print("*",end=" ")
+    print()    
+    
+  
+      
+
+        
+        
---
+++ main.py
@@ -0,0 +1,45 @@
+print("hi")
+#Question 1
+name=input("enter your name")
+clas=input("enter your class")
+section=input("enter your section")
+sub1=int(input("enter your marks for subject 1 out of 100 "))
+sub2=int(input("enter your marks for subject 2 "))
+sub3=int(input("enter your marks for subject 3 "))
+sub4=int(input("enter your marks for subject 4 "))
+sub5=int(input("enter your marks for subject 5 "))
+total=sub1+sub2+sub3+sub4+sub5
+percent=(total/500)*100
+print("name:",name)
+print("class:",clas)
+print("your total marks is",total)
+print("your scored percentage is",percent)
+#Question 2
+
+a=int(input("enter number 1"))
+b=int(input("enter number 2"))
+c=int(input("enter number 3"))
+sum=a+b+c
+print("sum of numbers:",sum)
+#Question 3
+num=int(input("enter a number"))
+sqr=num*num
+print(sqr)
+#Question 4
+temp=input("enter temp in celcius")
+temp1=float(temp)
+f=(temp1*9/5)+32
+print("temperature in celcius",temp1)
+print("temperature in farenheit",f)
+#Question 5
+a=int(input("enter value 1"))
+b=int(input("enter value 2"))
+q=a/b
+r=a%b
+print("quotient;",q)
+print("remainder:",r)
+
+
+
+
+
